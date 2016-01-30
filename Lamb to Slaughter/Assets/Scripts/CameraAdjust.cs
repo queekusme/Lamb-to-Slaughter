aad41ce70f14fe0b07074d35e00b978687384fcd
JayBean <jayhant1902@yahoo.com>
2016-01-30 03:34:56 +0000

    Base
    2 Players
    Adjusting Camera

---
+++ Lamb to Slaughter/Assets/Scripts/CameraAdjust.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAdjust : MonoBehaviour {
+
+    public GameObject sheep;
+    public GameObject shepherd;
+
+    public float cameraOffset;
+
+    public float minDistance;
+    public float maxDistance;
+
+    private Camera cam;
+
+    void Start() {
+        cam = GetComponent<Camera>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(getMiddlePoint(sheep, shepherd).x, getMiddlePoint(sheep, shepherd).y, cameraOffset);
+        cam.orthographicSize = Mathf.Clamp(getDistance(sheep, shepherd)/2,minDistance,maxDistance); ;
+    }
+
+    Vector2 getMiddlePoint(GameObject go1, GameObject go2) {
+        float x = ((go2.transform.position.x - go1.transform.position.x) / 2) + go1.transform.position.x;
+        float y = ((go2.transform.position.y - go1.transform.position.y) / 2) + go1.transform.position.y;
+
+        return new Vector2(x,y);
+    }
+
+    float getDistance(GameObject go1, GameObject go2) {
+        float x = go2.transform.position.x - go1.transform.position.x;
+        float y = go2.transform.position.x - go1.transform.position.x;
+
+        return Mathf.Sqrt((x * x) + (y * y));
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(getMiddlePoint(sheep, shepherd), 0.1f);
+    }
+
+}
